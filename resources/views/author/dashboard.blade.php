e40d627317cee28523a613ec0f5c0c505e41de32
Aboajah Emmanuel <aboajah.emmanuel@fmdqgroup.com>
2025-12-20 07:37:10 +0100

    Deployment

--- resources/views/author/dashboard.blade.php
+++ resources/views/author/dashboard.blade.php
@@ -45,7 +45,7 @@
             <div class="nk-block-head nk-block-head-sm">
                 <div class="nk-block-between">
                     <div class="nk-block-head-content">
-                        <h3 class="nk-block-title page-title">Author Dashboard</h3>
+                        <h3 class="nk-block-title page-title">Author Dashboard hhh</h3>
                         <div class="nk-block-des text-soft">
                             <p>Monitor your books, earnings, and publishing progress.</p>
                         </div>
